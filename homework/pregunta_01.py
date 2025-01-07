871f91ab4667df4620f1b748ff2f5d202942ef9e
jomolinav <jomolinav@unal.edu.co>
2025-01-06 19:10:33 -0500

    Lab3Casiqueno

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,19 +2,46 @@
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
-# pylint: disable=import-outside-toplevel
+import pandas as pd
+
+rutaarch = "files/input/clusters_report.txt"
+
+raw_data = pd.read_fwf(rutaarch, widths=[7, 16, 17, 100], names=["Cluster", "Cantidad de palabras clave", "Porcentaje de palabras clave", "Principales palabras clave"], skiprows=4  )
 
 
+consolidated_data = []
+current_cluster = None
+
+for _, row in raw_data.iterrows():
+    if not pd.isna(row["Cluster"]): 
+        if current_cluster:
+            consolidated_data.append(current_cluster)
+        current_cluster = {
+            "Cluster": int(row["Cluster"]),
+            "Cantidad de palabras clave": int(row["Cantidad de palabras clave"]),
+            "Porcentaje de palabras clave": row["Porcentaje de palabras clave"],
+            "Principales palabras clave": row["Principales palabras clave"].strip()
+        }
+    else:  
+        current_cluster["Principales palabras clave"] += " " + row["Principales palabras clave"].strip()
+
+
+if current_cluster:
+    consolidated_data.append(current_cluster)
+
+
+df_clusters = pd.DataFrame(consolidated_data)
+df_clusters.columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+df_clusters['porcentaje_de_palabras_clave'] = df_clusters['porcentaje_de_palabras_clave'].str.replace('%', '').str.replace(',', '.').astype(float, errors='ignore')
+
+df_clusters["principales_palabras_clave"] = (
+        df_clusters["principales_palabras_clave"]
+        .str.replace(r'\s+', ' ', regex=True)  
+        .str.replace(r',\s+', ', ') 
+        .str.strip()  # Q
+        .str.replace(".", "")
+    ) 
+
+#print(df_clusters.principales_palabras_clave.to_list()[0])
 def pregunta_01():
-    """
-    Construya y retorne un dataframe de Pandas a partir del archivo
-    'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
-
-    - El dataframe tiene la misma estructura que el archivo original.
-    - Los nombres de las columnas deben ser en minusculas, reemplazando los
-      espacios por guiones bajos.
-    - Las palabras clave deben estar separadas por coma y con un solo
-      espacio entre palabra y palabra.
-
-
-    """
+  return df_clusters
